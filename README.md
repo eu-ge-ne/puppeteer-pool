4508b30e0b43c0240af1b81cd8b28de066a2bd3b
Eugene Pobochny <eugene.pobochny@gmail.com>
2020-01-06 19:39:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# puppeteer-pool
+
+## About
+
+Page pooling for Puppeteer. Written in TypeScript. Each page is opened in new browser instance.
+
+## Install
+
+```bash
+$ npm install @eu-ge-ne/puppeteer-pool
+```
+
+## Example
+
+```typescript
+import { PuppeteerPool } from "@eu-ge-ne/puppeteer-pool";
+
+const pool = new PuppeteerPool({
+    launchOptions: {
+        ignoreHTTPSErrors: true,
+        timeout: 60000,
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-features=site-per-process",
+            "--disable-extensions",
+        ],
+    },
+    concurrency: 100,
+});
+
+let page: Page | undefined;
+
+try {
+    page = await pool.acquire();
+    page.on("error", err => console.log(err));
+    // ...
+} catch (err) {
+    // ...
+} finally {
+    if (page) {
+        await page.destroy(page);
+    }
+}
+```
+
+## API
+
+### Create instance
+
+```typescript
+import { PuppeteerPool } from "@eu-ge-ne/puppeteer-pool";
+
+const pool = new PuppeteerPool({
+    launchOptions: { }, // Puppeteer's LaunchOptions
+    concurrency: 1, // Pool's concurrency
+});
+```
+
+### Aquire page
+
+```typescript
+const page = await pool.acquire();
+```
+
+### Destroy page
+
+```typescript
+await page.destroy(page);
+```
+
+### Get status
+
+```typescript
+const [{ lifetime, counter, active }, ...rest] = pool.status();
+```
+
+Returns array of browser descriptors:
+
+ - `lifetime: number` - browser instance lifetime in ms
+ - `counter: number` - number of pages, opened by this browser instance
+    (max value is equal to `concurrency` parameter)
+ - `active: number` - number of active pages (browser instance will be
+    destroyed when `counter === concurrency` and `active === 0`)
+
+## License
+
+[MIT](LICENSE)
---
+++ index.ts
@@ -0,0 +1,107 @@
+import { Page, Browser, launch, LaunchOptions } from "puppeteer";
+import { Pool, createPool } from "generic-pool";
+import { LockAsync } from "@eu-ge-ne/lock-async";
+
+export type Options = {
+    launchOptions: LaunchOptions;
+    concurrency: number;
+};
+
+export type Status = Array<{
+    lifetime: number;
+    counter: number;
+    active: number;
+}>;
+
+type Item = {
+    created: number;
+    browser: Browser;
+    counter: number;
+    active: number;
+};
+
+export class PuppeteerPool {
+    private readonly items: Item[] = [];
+    private readonly pool: Pool<Page>;
+    private readonly lock = new LockAsync();
+
+    public constructor(private readonly options: Options) {
+        if (this.options.concurrency < 1) {
+            throw new Error("Concurrency option must be provided (>= 1)");
+        }
+
+        this.pool = createPool(
+            {
+                create: () => this.factoryCreate(),
+                destroy: (page: Page) => this.factoryDestroy(page),
+            },
+            {
+                max: this.options.concurrency,
+            });
+    }
+
+    public async stop() {
+        await this.pool.drain();
+        await this.pool.clear();
+
+        for (const item of this.items) {
+            await item.browser.close();
+        }
+    }
+
+    public status(): Status {
+        return this.items.map(x => ({
+            lifetime: Date.now() - x.created,
+            counter: x.counter,
+            active: x.active,
+        }));
+    }
+
+    private async factoryCreate(): Promise<Page> {
+        return await this.lock.run(async () => {
+            let item = this.items.find(x => x.counter < this.options.concurrency);
+
+            if (!item) {
+                const browser = await launch(this.options.launchOptions);
+                item = { created: Date.now(), browser, counter: 0, active: 0 };
+                this.items.push(item);
+            }
+
+            item.counter += 1;
+            item.active += 1;
+
+            return item.browser.newPage();
+        });
+    }
+
+    private async factoryDestroy(page: Page) {
+        await this.lock.run(async () => {
+            const browser = page.browser();
+            const index = this.items.findIndex(x => x.browser === browser);
+            if (index < 0) {
+                throw new Error("Browser not found in pool");
+            }
+
+            await page.close();
+
+            const item = this.items[index];
+
+            item.active -= 1;
+
+            if (item.counter >= this.options.concurrency) {
+                if (item.active === 0) {
+                    await item.browser.close();
+                    this.items.splice(index, 1);
+                }
+            }
+        });
+    }
+
+    public async acquire(): Promise<Page> {
+        return this.pool.acquire();
+    }
+
+    public async destroy(page: Page): Promise<void> {
+        await this.pool.destroy(page);
+    }
+}
